31ec87a3f1e160d1abc46cefc3a3352a3162498e
Kevin <knphillips01@gmail.com>
2020-12-07 17:24:41 -0600

    added 7 more problems

---
+++ minutesToSeconds.py
@@ -0,0 +1,16 @@
+"""
+Challenge #2:
+
+Write a function that takes an integer `minutes` and converts it to seconds.
+
+Examples:
+- convert(5) ➞ 300
+- convert(3) ➞ 180
+- convert(2) ➞ 120
+"""
+def convert(minutes):
+    # Your code here
+    return minutes * 60
+
+print( convert(5) )
+print( convert(3) )
---
+++ perimeterOfTriangle.py
@@ -0,0 +1,18 @@
+"""
+Challenge #4:
+
+Create a function that takes length and width and finds the perimeter of a
+rectangle.
+
+Examples:
+- find_perimeter(6, 7) ➞ 26
+- find_perimeter(20, 10) ➞ 60
+- find_perimeter(2, 9) ➞ 22
+"""
+def find_perimeter(length, width):
+    # Your code here
+    # 2l + 2w
+    return 2 * length + 2 * width
+
+
+print(find_perimeter(6,7) )
